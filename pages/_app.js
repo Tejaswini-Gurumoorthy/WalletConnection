ba852214315feb6e425ea78c2bddcf4749909cdb
Tejaswini-Gurumoorthy <75904755+Tejaswini-Gurumoorthy@users.noreply.github.com>
2022-12-20 21:34:20 +0530

    verified

--- pages/_app.js
+++ pages/_app.js
@@ -4,16 +4,17 @@ import { publicProvider } from 'wagmi/providers/public'; //like alchemy and infu
 import { SessionProvider } from 'next-auth/react';
 const defaultChains= [mainnet, goerli]
 
-const { provider, webSocketProvider } = configureChains(defaultChains, [publicProvider()]);
-const client = createClient({
+const {provider,webSocketProvider}= configureChains(defaultChains, [publicProvider()]);
+
+const client= createClient({
   provider,
   webSocketProvider,
   autoConnect: true,
-});
+})
 
 function MyApp({ Component, pageProps }) {
   return (
-    <WagmiConfig client={client}>
+    <WagmiConfig client={client}> 
       <SessionProvider session={pageProps.session} refetchInterval={0}>
         <Component {...pageProps} />
       </SessionProvider>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
 import MyApp from './_app'
 import styles from '../styles/Home.module.css'
+import axios from "axios"
+import { useState } from 'react';
 
 export default function Home() {
+  const [name, setName]= useState('');
+  async function getName()
+  {
+    const response= await axios.get('/api/hello');
+    setName(response.data.name)
+    
+    console.log(response.data.name);
+  }
+
   return (
     <div className={styles.container}>
-      <h1>Hello, World!</h1>
+      <button onClick={getName}>Get name</button>
+      <p>Your name is: {name}</p>
     </div>
   )
 }
